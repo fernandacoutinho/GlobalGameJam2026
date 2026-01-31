47f001cf2b8bd8537dcae7ed3f4333ff06694862
Fernanda Coutinho <fernanda.coutinho@estudante.ufjf.br>
2026-01-31 12:02:33 -0300

    :sparkles: adicona botões

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ function App() {
 
   return (
     <>
-      <h1>Hello, World!</h1>
+      <div className="app">
+        <h1>B.E.L.L. - Baile Encantado de Lala Land</h1>
+        <div className = "botoes">
+          <button>Jogar</button>
+          <button>Tutorial</button>
+        </div>
+      </div>
     </>
   )
 }
